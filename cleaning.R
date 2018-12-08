6bff25edd756c4b8cddf42c4693189ee62f066fa
Saransh-git <kumar.saransh@gmail.com>
2018-12-08 01:31:36 -0600

    cleaning.R changes

--- cleaning.R
+++ cleaning.R
@@ -14,44 +14,44 @@ if (FALSE){  # this block is skipped: we have no NA's in our data, YAYYY!!
   na.omit(members)
   na.omit(trans)
 }
-#################################################################################################
-# sanity checks
 
-train = train %>% dplyr::filter(nchar(msno) == 44) %>% dplyr::filter(endsWith(msno, '='))
 
 #################################################################################################
 # check and remove duplicates
-duplicated_train <- duplicated(train)
-duplicated_trans <- duplicated(trans)
-duplicated_members <- duplicated(members)
-duplicated_logs <-  duplicated(logs)
 
-if (any(duplicated_train == TRUE)){
-  train = as.tibble(train[-duplicated_train, ])
-}
+duplicate_train <- data_frame(duplicated = duplicated(train), row = 1:nrow(train)) %>% filter(duplicated == T)
+duplicate_logs <- data_frame(duplicated = duplicated(logs), row = 1:nrow(logs)) %>% filter(duplicated == T)
+duplicate_members <- data_frame(duplicated = duplicated(members), row = 1:nrow(members)) %>% filter(duplicated == T)
+duplicate_trans <- data_frame(duplicated = duplicated(trans), row = 1:nrow(trans)) %>% filter(duplicated == T)
 
-if (any(duplicated_logs == TRUE)){
-  logs = as.tibble(logs[-duplicated_logs, ])
-}
+p1 = ggplot(duplicate_train, aes(xintercept = row)) +
+  geom_vline(aes(xintercept = row)) + # plot a black line for each duplicated row
+  ggtitle("Duplicated train data") + # add a title
+  coord_flip() + scale_x_reverse() + xlab("row index")
 
-if (any(duplicated_members == TRUE)){
-  members = as.tibble(members[-duplicated_members, ])
-}
+p2 = ggplot(duplicate_logs, aes(xintercept = row)) +
+  geom_vline(aes(xintercept = row)) + # plot a black line for each duplicated row
+  ggtitle("Duplicated user logs data") + # add a title
+  coord_flip() + scale_x_reverse() + xlab("row index")
 
-if (any(duplicated_trans == TRUE)){
-  trans = as.tibble(trans[-duplicated_trans, ])
-}
+p3 =  ggplot(duplicate_members, aes(xintercept = row)) +
+  geom_vline(aes(xintercept = row)) + # plot a black line for each duplicated row
+  ggtitle("Duplicated members data") + # add a title
+  coord_flip() + scale_x_reverse() + xlab("row index")
 
-rm("duplicated_train", "duplicated_trans", "duplicated_members", "duplicated_logs")  # remove not 
+p4 = ggplot(duplicate_trans, aes(xintercept = row)) +
+  geom_vline(aes(xintercept = row)) + # plot a black line for each duplicated row
+  ggtitle("Duplicated transactions data") + # add a title
+  coord_flip() + scale_x_reverse() + xlab("row index")
+
+layout <- matrix(c(1,2,3,4),2,2,byrow=TRUE)
+multiplot(p1, p2, p3, p4, layout=layout)
+
+
+trans = as.tibble(trans[-duplicate_trans$row, ])
+
+rm("duplicate_train", "duplicate_trans", "duplicate_members", "duplicate_logs")  # remove not 
 #needed variables from workspace
-
-#################################################################################################
-# Keep only users present in train dataset
-
-trans = dplyr::filter(trans, msno %in% train$msno)
-logs = dplyr::filter(logs, msno %in% train$msno)
-members = dplyr::filter(members, msno %in% train$msno)
-
 
 #################################################################################################
 # Feature transformation
@@ -64,7 +64,7 @@ members <- members %>% mutate(
   registration_init_time = ymd(registration_init_time)
 )
 
-trans <- trans %>% select(-plan_list_price) %>%  # dropping listed price as against actual price paid
+trans <- trans %>%  # dropping listed price as against actual price paid
   mutate(
     payment_method_id = factor(payment_method_id),  # converting to factor as unordered
     is_auto_renew = factor(is_auto_renew),  # converting to factor as unordered
@@ -75,6 +75,46 @@ trans <- trans %>% select(-plan_list_price) %>%  # dropping listed price as agai
 
 logs <- logs %>%
   mutate(date = ymd(date))
+
+corrplot(cor(data.matrix(members %>% select(-msno, -gender))), method="pie")
+corrplot(cor(data.matrix(logs %>% select(-msno))), method="pie")
+corrplot(cor(data.matrix(trans %>% select(-msno))), method="pie")
+
+p1 = ggplot(data=logs) + geom_boxplot(mapping=aes(x="", y=num_25)) + xlab("") + coord_flip()
+p2 = ggplot(data=logs) + geom_boxplot(mapping=aes(x="", y=num_50)) + xlab("") + coord_flip()
+p3 = ggplot(data=logs) + geom_boxplot(mapping=aes(x="", y=num_75)) + xlab("") + coord_flip()
+p4 = ggplot(data=logs) + geom_boxplot(mapping=aes(x="", y=num_985)) + xlab("") + coord_flip()
+
+layout <- matrix(c(1,2,3,4),2,2,byrow=TRUE)
+multiplot(p1, p2, p3, p4, layout=layout)
+
+p1 = ggplot(data=logs) + geom_boxplot(mapping=aes(x="", y=num_100)) + xlab("") + coord_flip()
+p2 = ggplot(data=logs) + geom_boxplot(mapping=aes(x="", y=num_unq)) + xlab("") + coord_flip()
+p3 = ggplot(data=logs) + geom_boxplot(mapping=aes(x="", y=total_secs)) + xlab("") + coord_flip()
+
+layout <- matrix(c(1,2,3),3,1,byrow=TRUE)
+multiplot(p1, p2, p3, layout=layout)
+
+p1 = ggplot(data=members) + geom_boxplot(mapping=aes(x="", y=registration_init_time)) + xlab("") + ylab("members$registration_init_time") + coord_flip()
+p2 = ggplot(data=logs) + geom_boxplot(mapping=aes(x="", y=date)) + xlab("") + ylab("logs$date") + coord_flip()
+layout <- matrix(c(1,2),2,1,byrow=TRUE)
+multiplot(p1, p2, layout=layout)
+
+p1 = ggplot(data=trans) + geom_boxplot(mapping=aes(x="", y=actual_amount_paid)) + xlab("") + ylab("transactions$actual_amount_paid") + coord_flip()
+p2 = ggplot(data=trans) + geom_boxplot(mapping=aes(x="", y=plan_list_price)) + xlab("") + ylab("transactions$plan_list_price") + coord_flip()
+layout <- matrix(c(1,2),2,1,byrow=TRUE)
+multiplot(p1, p2, layout=layout)
+
+members = members %>% filter(bd>=0, bd<=100) %>% filter(registration_init_time>=20080000)
+logs = logs %>% filter(num_25>=0, num_25<=1000) %>% filter(num_50>=0, num_50<=500) %>%
+  filter(num_75>=0, num_75<=200) %>% filter(num_985>=0, num_985<=500) %>% filter(num_100>=0, num_100<=5000) %>%
+  filter(num_unq>=0, num_unq<=1000) %>% filter(total_secs>=-2.5e+15, total_secs<=2.5e+15)
+trans = trans %>% filter(actual_amount_paid>=0, actual_amount_paid<=1000) %>% filter(plan_list_price>=0, plan_list_price<=1000)
+
+#################################################################################################
+# sanity checks
+
+train = train %>% dplyr::filter(nchar(msno) == 44) %>% dplyr::filter(endsWith(msno, '='))
 
 #################################################################################################
 # Missing value imputation
@@ -94,7 +134,22 @@ knn_impute_missing_gender_from_members = function(){
 
 members = knn_impute_missing_gender_from_members()
 
+corrplot(cor(data.matrix(members %>% select(-msno, -gender))), method="pie")
+corrplot(cor(data.matrix(logs %>% select(-msno))), method="pie")
+corrplot(cor(data.matrix(trans %>% select(-msno))), method="pie")
+
+trans <- trans %>% select(-plan_list_price)
+
+#################################################################################################
+# Keep only users present in train dataset
+
+trans = dplyr::filter(trans, msno %in% train$msno)
+logs = dplyr::filter(logs, msno %in% train$msno)
+members = dplyr::filter(members, msno %in% train$msno)
+
+
 sample = inner_join(train, members, by= ("msno"="msno")) %>% 
   inner_join(trans, by=c("msno"="msno")) %>% 
   inner_join(logs, by=c("msno"="msno"))
+
 
